bfb93faf9410274e78d3cf8bbefb840130f1015e
Nikita Vaidya <vaidyanikita99@gmail.com>
2020-01-24 18:36:42 +0530

    changes in config/database

--- config/database.js
+++ config/database.js
@@ -1,17 +1,16 @@
 const mongoose=require('mongoose')
-
+const CONNECTION = process.env.MONGODB_URL || 'mongodb://localhost:27017/ticket-master-backend'
 const configureDB=()=>{
     //db configuration
-    // mongoose.connect('mongodb://localhost:27017/ticket-master-backend', {
-    //     useNewUrlParser:true,
-    //     useUnifiedTopology:true
-    // })
-
-   
-    mongoose.connect('mongodb+srv://nikita_21:nikita123@cluster0-wldti.mongodb.net/test?retryWrites=true&w=majority', {
+    mongoose.connect(CONNECTION, {
         useNewUrlParser:true,
         useUnifiedTopology:true
     })
+   
+    // mongoose.connect('mongodb+srv://nikita_21:nikita123@cluster0-wldti.mongodb.net/test?retryWrites=true&w=majority', {
+    //     useNewUrlParser:true,
+    //     useUnifiedTopology:true
+    // })
     .then(()=>{
         console.log('connected to db (ticket master)')
     })
